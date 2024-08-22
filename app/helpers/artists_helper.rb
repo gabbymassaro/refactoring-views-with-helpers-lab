74e3d2c67835d0b8c9e4f283d2d6ce8fefe54404
Gabriella Massaro <gmassaro807@gmail.com>
2024-08-21 22:59:21 -0400

    EOD commit

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,5 +1,9 @@
 module ArtistsHelper
   def artist_name(song)
-    song.artist_name
+    song.artist_name if song.artist.present?
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
   end
 end
